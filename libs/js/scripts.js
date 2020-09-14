30a7b95dafad15ba2b9dc6399817bd14216ddfa4
Pez-Gordo <62446883+Pez-Gordo@users.noreply.github.com>
2020-09-14 15:28:07 +0100

    Add files via upload

--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -1,41 +1,81 @@
-// In the event of a click in the button with ID of #api we trigger an anonnymous function
-$("#api").click(function () {
-  
-  // We store latitude and longitude values in the "dataToSend variable"
-  var dataToSend = {
-    lat: $("#selLat").val(),
-    lng: $("#selLng").val(),
-  };
-  // Here starts our asynchronous JavaScript code, but instead of XML we are using JSON
+$("#api1, #api2, #api3, #api4, #api5").click(function () {
+  //Pulls the apiNum variable from the button triggering the click event
+  var button = this.id;
+
+  //Sets the data to send depending on the API used.
+  if (button === "api1") {
+    var dataToSend = {
+      apiNum: "api1",
+      country: $("#selCountry").val(),
+    };
+  } else if (button === "api2") {
+    var dataToSend = {
+      apiNum: "api2",
+      lat: $("#selLat").val(),
+      lng: $("#selLng").val(),
+    };
+  } else if (button === "api3") {
+    var dataToSend = {
+      apiNum: "api3",
+      lat: $("#selLat").val(),
+      lng: $("#selLng").val(),
+    };
+  } else if (button === "api4") {
+    var dataToSend = { apiNum: "api4", q: $("#selLocation").val() };
+  } else if (button === "api5") {
+    var dataToSend = {
+      apiNum: "api5",
+      lat: $("#selLat").val(),
+      lng: $("#selLng").val(),
+    };
+  }
+
   $.ajax({
-    // Parameters
     url: "libs/php/functions.php",
     type: "POST",
     dataType: "json",
     data: dataToSend,
-    // In case of success
     success: function (result) {
-
       console.log(result);
-
       if (result.status.name == "ok") {
-
-        // We use the empty() method to erase any existing table
+        //Removes any existing table
         $("#results").find("tbody").empty();
 
-        // We loop the JSON and execute the iterate function on each item
+        //Loops the JSON depending on whether it contains an array or object
         $.each(result, function (key, value) {
-            $.each(value, iterate);
+          if (Array.isArray(value)) {
+            for (var i = 0; i < value.length; i++) {
+              $.each(value[i], JSONIterator);
+              //Adds an empty row between results
+              $("#results tbody").append(
+                '<tr><td colspan="2">' + "" + "</td></tr>"
+              );
+            }
+          } else {
+            $.each(value, JSONIterator);
+          }
         });
-        // The iterate function builds the table with the results
-        function iterate(key, value) {
-          if (key !== "code" && key !== "name" && key !== "description" && key !== "returnedIn") {
-            $("#results tbody").append("<tr><td>" + key + "</td><td> ----> </td><td>" + value + "</td></tr>");
+
+        function JSONIterator(key, value) {
+          if (value != null && typeof value == "object") {
+            // Recurse into children
+            $.each(value, JSONIterator);
+          } else {
+            //Removes request info from JSON and adds to results table
+            if (
+              key !== "code" &&
+              key !== "name" &&
+              key !== "description" &&
+              key !== "returnedIn"
+            ) {
+              $("#results tbody").append(
+                "<tr><td>" + key + "</td><td>" + value + "</td></tr>"
+              );
+            }
           }
         }
       }
     },
-    // In case of error...
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(jqXHR, textStatus, errorThrown);
     },
